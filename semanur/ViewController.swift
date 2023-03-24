f7d5785120608ed97a83b1369c05b288f37439d8
suhaKarakaya <suhabskarakaya@gmail.com>
2023-03-24 14:34:40 +0300

    +++

--- semanur/ViewController.swift
+++ semanur/ViewController.swift
@@ -21,56 +21,56 @@ class ViewController: UIViewController {
         tableView.dataSource = self
 //        deneme
         
-        //        AF.request("https://ezanvakti.herokuapp.com/vakitler/9541", method: .get, encoding: URLEncoding.default, headers: nil, interceptor: nil).response { (responseData) in
-        //
-        //            guard let data = responseData.data else { return }
-        //
-        //            do{
-        //                let data = try JSONDecoder().decode([Country].self, from: data)
-        //                self.tableViewlist = data
-        //                self.tableView.reloadData()
-        //            } catch {
-        //            }
-        //
-        //        }
-        //
-        //        let url = URL(string: "https://ezanvakti.herokuapp.com/vakitler/9541")!
-        //
-        //        URLSession.shared.dataTask(with: url) { data, httpUrlResponse, error in
-        //
-        //            guard let data = data else { return }
-        //
-        //            do{
-        //                let data = try JSONDecoder().decode([Country].self, from: data)
-        //                debugPrint(data)
-        //            } catch let error {
-        //                print("Error: ", error)
-        //            }
-        //        }.resume()
+                AF.request("https://ezanvakti.herokuapp.com/vakitler/9541", method: .get, encoding: URLEncoding.default, headers: nil, interceptor: nil).response { (responseData) in
         
-//        let parameters :[String: Any] = [
-//            "token": "SIM5QtmcpEu7fVT+pD2xU3GmR0m3L5m+NBR1cYB0O6Irij+u1KX0NFpSr/bRIuHi~1380~CAC10606-6B0E-4924-9D6F-B58C678CEA62~638152632412676079",
-//            "method": "profile",
-//            "language": "TR"
-//        ]
-//
-//
-//
-//        AF.request("https://mdtest.limakcimento.com/cevik/api/gateway", method: .post, parameters: parameters, encoding: JSONEncoding.default).validate().responseDecodable(of : Welcome.self) { response in
-//            guard let model = response.value
-//            else {print(response.error as Any); return }
-//            debugPrint(model)
-//
-//        }
+                    guard let data = responseData.data else { return }
         
-        let parameterData = Model(token: "J5gj2Xmu3zvuOnJ2QDLbORi2QUSRPBadQ5zyyUKnHlJ9beG/QMn/rxo05nS9Yiv+~1380~CAC10606-6B0E-4924-9D6F-B58C678CEA62~638152527255693078", language: "TR", method: "profile")
+                    do{
+                        let data = try JSONDecoder().decode([Country].self, from: data)
+                        self.tableViewlist = data
+                        self.tableView.reloadData()
+                    } catch {
+                    }
+        
+                }
+        
+                let url = URL(string: "https://ezanvakti.herokuapp.com/vakitler/9541")!
+        
+                URLSession.shared.dataTask(with: url) { data, httpUrlResponse, error in
+        
+                    guard let data = data else { return }
+        
+                    do{
+                        let data = try JSONDecoder().decode([Country].self, from: data)
+                        debugPrint(data)
+                    } catch let error {
+                        print("Error: ", error)
+                    }
+                }.resume()
+        
+        let parameters :[String: Any] = [
+            "token": "SIM5QtmcpEu7fVT+pD2xU3GmR0m3L5m+NBR1cYB0O6Irij+u1KX0NFpSr/bRIuHi~1380~CAC10606-6B0E-4924-9D6F-B58C678CEA62~638152632412676079",
+            "method": "profile",
+            "language": "TR"
+        ]
 
-        ServiceManager.shared.fetch(path: "https://mdtest.limakcimento.com/cevik/api/gateway", parameterModel: parameterData)
-        { (response: [ProfileBaseResponse]) in
-            debugPrint(response)
-        } onError: { error in
-            debugPrint(error)
+
+
+        AF.request("https://mdtest.limakcimento.com/cevik/api/gateway", method: .post, parameters: parameters, encoding: JSONEncoding.default).validate().responseDecodable(of : Welcome.self) { response in
+            guard let model = response.value
+            else {print(response.error as Any); return }
+            debugPrint(model)
+
         }
+        
+//        let parameterData = Model(token: "J5gj2Xmu3zvuOnJ2QDLbORi2QUSRPBadQ5zyyUKnHlJ9beG/QMn/rxo05nS9Yiv+~1380~CAC10606-6B0E-4924-9D6F-B58C678CEA62~638152527255693078", language: "TR", method: "profile")
+//
+//        ServiceManager.shared.fetch(path: "https://mdtest.limakcimento.com/cevik/api/gateway", parameterModel: parameterData)
+//        { (response: [ProfileBaseResponse]) in
+//            debugPrint(response)
+//        } onError: { error in
+//            debugPrint(error)
+//        }
 
         
         
